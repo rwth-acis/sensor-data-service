bb15dc0150bcedbe27fd7535906191a7c4d2ad98
Philipp <philipp.roytburg@rwth-aachen.de>
2019-08-25 20:11:33 +0200

    Initialize

--- README.md
+++ README.md
@@ -1,1 +1,70 @@
-sensor-data-service
+Sensor Data Service
+===========================================
+This repository is part of the bachelor thesis 'A Multimodal Mentoring Cockpit for Tutor Support'.
+It is a las2peer Service, that enables the connection between [ARLEMPanel](https://github.com/rwth-acis/ARLEMPanel) database and Learning Locker.
+Other related repositories for the bachelor thesis can be found here: [Mentoring Cockpit](https://github.com/rwth-acis/Mentoring-Cockpit)
+
+Sensor data
+-----------
+Actions performed in the [ARLEMPanel](https://github.com/rwth-acis/ARLEMPanel) are stored by the [msfusion.net](https://github.com/rwth-acis/msfusion.net) context manager using [ARLEMServices](https://github.com/rizalishan/ARLEMServices) componentes.
+The sensor data service connects directly to the database used by [ARLEMServices](https://github.com/rizalishan/ARLEMServices) and [ARLEMPanel](https://github.com/rwth-acis/ARLEMPanel) and selects all the necessary data for xAPI statements, that represent actions performed by persons.
+Then the service creates the statements and offers a RESTful POST request to send them to the configured LRS instance.
+
+
+To send the statements to the LRS:
+```
+POST <service-address>/sensor/sendData
+```
+
+Therefore, replace *service-address* with the address the service is running on.
+
+
+Learning Locker configuration
+--------------------------
+In Learning Locker an LRS is configured under Settings > Store > Add new.
+And under Settings > Client a corresponding client can be configured with the authentication.
+
+Service setup
+-------------
+To set up the service configure the [property file](etc/i5.las2peer.services.sensorDataService.SensorDataService.properties) file with the database credentials and Learning Locker authentication.
+```INI
+lrsDomain = http://exampleDomain/data/xAPI/statements
+lrsAuth = Basic exampleauth
+mysqlUser = exampleuser
+mysqlPassword = examplepass
+mysqlHost = localhost
+mysqlPort = 3306
+mysqlDatabase = exampledb
+```
+
+Build
+--------
+Execute the following command on your shell:
+
+```shell
+ant jar 
+```
+
+Start
+--------
+
+To start the sensor-data service, follow the [Starting-A-las2peer-Network tutorial](https://github.com/rwth-acis/las2peer-Template-Project/wiki/Starting-A-las2peer-Network) and bootstrap your service to a [mobsos-data-processing service](https://github.com/rwth-acis/mobsos-data-processing/tree/bachelor-thesis-philipp-roytburg).
+
+How to run using Docker
+-------------------
+
+First build the image:
+```bash
+docker build . -t learning-locker
+```
+
+Then you can run the image like this:
+
+```bash
+docker run -e LRS_DOMAIN=lrsDomain -e LRS_AUTH=lrsAuth -e MYSQL_USER=mysqlUser -e MYSQL_PASSWORD=mysqlPassword -e MYSQL_HOST=mysqlHost -e MYSQL_PORT=mysqlPort -e MYSQL_DATABASE=mysqlDatabase -p 9011:9011 mentoring-cockpit-service
+```
+
+Replace *lrsDomain* with your Learning Locker domain, *lrsAuth* with the corresponding authentication, *mysqlUser* with the MySQL user name, *mysqlPassword* with the MySQL password, *mysqlHost* with the MySQL host, *mysqlPort* with the MySQL port, and *mysqlDatabase* with the MySQL database name. 
+
+*Do not forget to persist you database data*
+
---
+++ src/main/i5/las2peer/services/sensorDataService/SensorDataService.java
@@ -0,0 +1,261 @@
+package i5.las2peer.services.sensorDataService;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import i5.las2peer.api.ManualDeployment;
+import i5.las2peer.restMapper.RESTService;
+import i5.las2peer.restMapper.annotations.ServicePath;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.Contact;
+import io.swagger.annotations.Info;
+import io.swagger.annotations.SwaggerDefinition;
+import net.minidev.json.JSONObject;
+
+/**
+ * Sensor-Data-Service
+ * 
+ * This is a service for the connection between the ARLEM services database and Learning Locker.
+ * This service connects with the MySQL database of the ARLEM services, which is responsible for
+ * for storing learner traces measured by the multisensor data fusion framework.
+ * Then identifies and retrieves the data important for the analytics part and then based on that 
+ * creates xAPI statements and sends them to an LRS
+ */
+@Api
+@SwaggerDefinition(
+		info = @Info(
+				title = "Sensor-Data-Service",
+				version = "1.0",
+				description = "A service for the connection between the ARLEM services database and Learning Lockers.",
+				contact = @Contact(
+						name = "Philipp Roytburg",
+						email = "philipp.roytburg@rwth-aachen.de")))
+
+@ManualDeployment
+@ServicePath("/sensor")
+public class SensorDataService extends RESTService {
+
+	private String lrsDomain;
+	private String lrsAuth;
+	private String mysqlUser;
+	private String mysqlPassword;
+	private String mysqlHost;
+	private String mysqlPort;
+	private String mysqlDatabase;
+	
+	private Connection con;
+	private ArrayList<String> oldstatements = new ArrayList<String>();
+	
+	/**
+	 * 
+	 * Constructor of the Service. Sets values of the configuration file.
+	 * 
+	 */
+	public SensorDataService() {
+		setFieldValues();
+		
+	}
+	
+	
+	/**
+	 * A function that is called by the user to send sensor data to the LRS 
+	 * It firsts tries to connect to the database and then gets the statements and sends them to an LRS
+	 * 
+	 * @return a response message if everything went ok
+	 * 
+	 */
+	@POST
+	@Path("/sendData")
+	@Produces(MediaType.TEXT_PLAIN)
+	@ApiResponses(
+			value = { @ApiResponse(
+					code = HttpURLConnection.HTTP_OK,
+					message = "Moodle connection is initiaded") })
+	public Response sendSensorData() {
+		// connect with the database
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con=DriverManager.getConnection("jdbc:mysql://" + mysqlHost + ":" + mysqlPort + "/" + mysqlDatabase,
+					mysqlUser, mysqlPassword);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		ArrayList<String> newstatements = getSensorData();
+		sendXAPIstatements(newstatements);
+		
+		try {
+			con.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return Response.ok().entity("Sensor data was sent to LRS").build();
+	}
+	
+	/**
+	 * This function is called by the sendSensorData function. 
+	 * It tries to an sql query to get the data from the database 
+	 * and then calls statementGenerator function to create statements out of the data.
+	 * 
+	 * @return an Arraylist of statements
+	 * 
+	 */
+	private ArrayList<String> getSensorData() {
+		ArrayList<String> newstatements = new ArrayList<String>();
+		try {
+			String query = "select b.personName, b.personMbox, actions.id as actionId, actions.name as actionName, actions.instructionDescription "
+					+ "from actions "
+					+ "join ( "
+					+ "select a.personName, a.personMbox, action_triggers.action "
+					+ "from action_triggers "
+					+ "join ( "
+					+ "select persons.name as personName, persons.mbox as personMbox, action_trigger_operations.actionTrigger "
+					+ "from persons join action_trigger_operations "
+					+ "on action_trigger_operations.entityId = persons.id "
+					+ "where action_trigger_operations.entityType = 'Person' ) "
+					+ "as a on a.actionTrigger = action_triggers.id ) "
+					+ "as b on b.action = actions.id;";
+			Statement stmt=con.createStatement();
+			ResultSet rs=stmt.executeQuery(query);
+			while(rs.next()) {
+				String personName = rs.getString("personName");
+				String personMbox = rs.getString("personMbox");
+				String actionId = rs.getString("actionId");
+				String actionName = rs.getString("actionName");
+				String instructionDescription = rs.getString("instructionDescription");
+				
+				String statement = statementGenerator(personName, personMbox, actionId, actionName, instructionDescription);
+				
+				newstatements.add(statement.toString());
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return newstatements;
+	}
+	
+	/**
+	 * This function is called by the getSensorData function. 
+	 * It gets the relevant data to create an xAPI statement out of Sensor data 
+	 * and creates a statement as a JSONObject.
+	 *
+	 * @param personName the name of a person who performed an action
+	 * @param personMbox the email of a person who performed an action
+	 * @param actionId the id of an action that was performed
+	 * @param actionName the name of an action that was performed
+	 * @param instructionDescription the instruction description of an action that was performed
+	 * 
+	 * @return an xAPI statement in string format
+	 * 
+	 */
+	private String statementGenerator(String personName, String personMbox, String actionId, String actionName, String instructionDescription) {
+		JSONObject statement = new JSONObject();
+		
+		JSONObject actor = new JSONObject();
+		actor.put("name", personName);
+		actor.put("mbox", "mailto:" + personMbox);
+		statement.put("actor", actor);
+		
+		JSONObject verb = new JSONObject();
+		verb.put("id", "http://example.com/xapi/performed");
+		JSONObject display = new JSONObject();
+		display.put("en-US", "performed");
+		verb.put("display", display);
+		statement.put("verb", verb);
+		
+		JSONObject object = new JSONObject();
+		object.put("id", "http://example.com/actionId=" + actionId);
+		JSONObject definition = new JSONObject();
+		JSONObject name = new JSONObject();
+		name.put("en-US", actionName);
+		definition.put("name", name);
+		if (instructionDescription != null && !instructionDescription.equals("")) {
+			JSONObject description = new JSONObject();
+			description.put("en-US", instructionDescription);
+			definition.put("description", description);
+		}
+		object.put("definition", definition);
+		statement.put("object", object);
+		
+		//TODO if you find results
+		/*
+		JSONObject result = new JSONObject();
+		JSONObject score = new JSONObject();
+		score.put("score", value)
+		*/
+		
+		return statement.toString();
+	}
+	
+	
+	/**
+	 * A function that is called by sendSensorData.
+	 * It sends Sensor data to the LRS if it wasn't already sent.
+	 *
+	 * @param statements the statements that are being sent to an LRS
+	 * 
+	 */
+	public void sendXAPIstatements(ArrayList<String> statements) {
+		for(String statement : statements) {
+			if(!oldstatements.contains(statement)) {
+				oldstatements.add(statement);
+
+				try {
+					URL url = new URL(lrsDomain);
+					HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+					conn.setDoOutput(true);
+					conn.setDoInput(true);
+					conn.setRequestMethod("POST");
+					conn.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+					conn.setRequestProperty("X-Experience-API-Version","1.0.3");
+					conn.setRequestProperty("Authorization", lrsAuth);
+					conn.setRequestProperty("Cache-Control", "no-cache");
+					conn.setUseCaches(false);
+					
+					OutputStream os = conn.getOutputStream();
+					os.write(statement.getBytes("UTF-8"));
+					os.flush();
+
+					Reader reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+					
+					for (int c; (c = reader.read()) >= 0;)
+						System.out.print((char)c);
+					
+					conn.disconnect();
+				} catch (MalformedURLException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+
+		}
+	}
+
+
+}
